0642662aa33eb56f81d44a1f54879b09a9f56a06
mshzidan22 <40596471+mshzidan22@users.noreply.github.com>
2023-09-29 21:07:55 +0300

    Update environment.prod.ts with aws url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://olx1.herokuapp.com',
+  BASE_URL: 'http://http://51.20.81.246',
   IMG_API :'https://api.sirv.com/v2/files/upload'
 };
